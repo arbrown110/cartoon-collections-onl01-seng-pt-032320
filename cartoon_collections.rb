b3bd5b07d2760e3f78e60a65d91cf6539b11c392
chic-caller-7520 <arbrown110@gmail.com>
2020-04-02 02:28:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,26 +1,27 @@
-def roll_call_dwarves(dwarfs)
-  dwarfs.each_with_index do |dwarf , ind|
-    binding.pry
-    puts " #{ind + 1 }.#{dwarfs} "
-end
 
-def summon_captain_planet (planteer_calls)
-  planteer_calls.map do { |planteer_calls.capitalize + "!" |}
     
+def roll_call_dwarves(array)
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def long_planeteer_calls (call)
-    planteer_calls.any? |call| call.length > 4 |
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+  end
+  planeteer_calls
 end
 
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+end
+end
 
-
-
-
-def find_the_cheese (maybe_cheese)
-  matbe_cheesy.find do |type_cheese|
-    cheeses.include?(cheese)
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
 end
-    
-find_the_cheese(ingredients)
+
