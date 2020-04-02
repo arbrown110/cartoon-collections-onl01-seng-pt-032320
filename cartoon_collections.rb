48902d01d71317c58183728019f54e7b101d7913
chic-caller-7520 <arbrown110@gmail.com>
2020-04-02 02:23:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,10 @@ def long_planeteer_calls (call)
     planteer_calls.any? |call| call.length > 4 |
 end
 
+
+
+
+
 def find_the_cheese (cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   potentially_cheesy_items.find do |cheese|
