aeaa059ea34209b2c64c09501e33f18692ca154e
chic-caller-7520 <arbrown110@gmail.com>
2020-04-02 02:21:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index do |dwarf , ind|
+    binding.pry
+    puts " #{ind + 1 }.#{dwarfs} "
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planteer_calls)
+  planteer_calls.map do { |planteer_calls.capitalize + "!" |}
+    
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (call)
+    planteer_calls.any? |call| call.length > 4 |
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  potentially_cheesy_items.find do |cheese|
+    cheeses.include?(cheese)
+  end
 end
+    
+
